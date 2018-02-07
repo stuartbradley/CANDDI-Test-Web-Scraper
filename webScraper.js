c5c8feeb234ca03f403d736ea8c74e1e8afa9558
stuartbradley <tuey_14@hotmail.co.uk>
2018-02-07 20:17:37 +0000

    Update webScraper.js

--- webScraper.js
+++ webScraper.js
@@ -1,25 +1,15 @@
 
 //Setting Requirements
-const prompt = require('prompt');
 const request = require('request');
 const cheerio = require('cheerio');
-const sleep = require('system-sleep');
 const Knwl = require('knwl.js');
+var readlineSync = require('readline-sync');
 var knwlInstance = new Knwl('english');   
 
-//prompts for email address and formats to get domain name.
-prompt.start();
-prompt.get(['email'], function(err,result){
-	console.log('command-line input recieved: ');
-	console.log('email: ' + result.email)
-	var email = result.email;
-	const domain = email.replace(/.*@/, "");
+	var userName = readlineSync.question('please enter your email account ');
+	const domain = userName.replace(/.*@/, "");
 	site = 'http://www.' + domain;
-  console.log("Scraping information from " + site);
-},);
-
-//gives user 10 seconds to enter email before next piece of code runs
-sleep(10000);
+	
 
 //sends request to website and checks for a 200 response. If there is no 200 status code, program finishes.
 request(site, function (error, response, body) {
@@ -33,5 +23,3 @@ request(site, function (error, response, body) {
 	  console.log(site + " cannot be reached, please restart the program and try again");
   }
 });
-
-
