ad08e9282903c1357c7995e258378b325618753f
stuartbradley <tuey_14@hotmail.co.uk>
2018-02-08 18:21:55 +0000

    Update webScraper.js

--- webScraper.js
+++ webScraper.js
@@ -4,16 +4,10 @@ const request = require('request');
 const cheerio = require('cheerio');
 var readlineSync = require('readline-sync');        
 var firstline = require('firstline');
-var Knwl = require('knwl.js');
-var knwlInstance = new Knwl('english');
-knwlInstance.register('internationalPhones', require('./node_modules/knwl.js/experimental_plugins/internationalPhones'));
-
-
 
 var PhoneNumberReg = "(\\+\\d{1,3}\\s?(\\s\\(0\\))?|0)(\\d{3}\\s?\\d{3}\\s?\\d{4}|\\d{4}\\s?\\d{6})(?![0-9])";
 var emailReg = "[A-Za-z][A-Za-z0-9._%+-]+@[A-Za-z0-9-]+(\\.[A-Za-z]{3}|(\\.[A-Za-z]{2}){2})";
-var AddressReg ="([A-Z]+\\s)*(\\d{1,3}-)?\\d*\\s([A-Z]+,?\\s)+(street|road|avenue|square|lane|grove|fair|hill|drive|circle|crescent|boulevard|parade|close|court|exchange|st|rd|av),?\\s?(([A-Z]+)?,?\\s?){1,2}([A-Z]{1,2}(([0-9]{1,2})|([0-9][A-Z]))\\s[0-9][A-Z]{1,2})";
-
+var postCodeReg = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})";
 
 	//asks user for email and takes domain name from email
 	var emailAccount = readlineSync.question('please enter your email account ');
@@ -26,25 +20,22 @@ request(site, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(body);
 	var html = $.html();
-	knwlInstance.init(html);
-	var phoneNumber = knwlInstance.get('internationalPhones');
-	console.log(phoneNumber);
-	scrapeDate(html,PhoneNumberReg);
-	scrapeDate(html,emailReg);
-	scrapeDate(html,AddressReg);
+	scrapeDate(html,PhoneNumberReg,"phone number");
+	scrapeDate(html,emailReg, "email address");
+	scrapeDate(html,postCodeReg, "postcode");
   }else{
 	  console.log(site + " cannot be reached, please restart the program and try again");
   }
 });
 
 //formats scrape from site using regex;
-function scrapeDate(scrape, reg){
+function scrapeDate(scrape, reg, info){
 	var matches = scrape.match(reg);
 if(matches === null){
-	console.log("cannot find address");
+	console.log("cannot find address " + info);
 }else{
-	var information = matches.slice(0,1);
-	console.log(information);
+	var slicedData = matches.slice(0,1);
+	console.log("the " + info + " is " + slicedData);
 }
 };
 
